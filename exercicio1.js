d2bf8fb3ff2aa771e32ec886d682421c874557a3
mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2024-08-22 20:04:49 -0300

    feat: Exercicio 1 concluido

--- exercicio1.js
+++ exercicio1.js
@@ -11,21 +11,36 @@ Obs use o promt e exiba os resultado em alert.
 */
 
 let nome1 = "";
-let nota1, nota2, nota3
+let nota1, nota2, nota3;
 
 nome1 = prompt("Digite o Nome do Aluno 1: ");
+
 nota1 = parseFloat(prompt("Digite a Nota 1: "));
+
 nota2 = parseFloat(prompt("Digite a Nota 2: "));
+
 nota3 = parseFloat(prompt("Digite a Nota 3: "));
 
 let media = 0;
 media = (nota1 + nota2 + nota3) / 3;
 
-let mensagem = `
-${nome1}, sua média é ${media1}
-`
-if (media >= 6) {
-    alert(mensagem);("Aprovado");
+let mensagem = "";
+
+if (media >= 7) {
+    mensagem = ("Aprovado");
+
+} else {
+
+    if (media >= 5) {
+        mensagem = ("Reprovado");
+
+    } else {
+
+        mensagem = ("Recuperação");
+
+    }
 }
+
+alert(mensagem);
 
 
