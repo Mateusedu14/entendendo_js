9c67de70ee1a9c5f86553f1c77b20b58a840e70e
mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2024-08-22 22:03:04 -0300

    feat: exercício 2 concluído

--- exercicio2.js
+++ exercicio2.js
@@ -11,25 +11,23 @@ conforme abaixo
 6. Sexta-feira
 7. Sábado
 */
-let dia_escolhido = prompt("Informe o numero do dia da semana a exibir: ");
-let mensagem = "";
+let dia_escolhido = 
+parseInt(prompt("Informe o numero do dia da Semana a exibir:"));
 
-if (dia_escolhido == 1) {
-    alert("O Dia escolhido é DOMINGO!!");
-  
-
-} else if (dia_escolhido == 2) {
-
-} else if (dia_escolhido == 3) {
-
-} else if (dia_escolhido == 4) {
-
-} else if (dia_escolhido == 5) {
-
-} else if (dia_escolhido == 6) {
-
-} else if () {
-
+if (dia_escolhido === 1) {
+    alert("O dia escolhido é Domingo");
+} else if (dia_escolhido === 2) {
+    alert("O dia escolhido é Segunda");
+} else if (dia_escolhido === 3) {
+    alert("O dia escolhido é Terça");
+} else if (dia_escolhido === 4) {
+    alert("O dia escolhido é Quarta");
+} else if (dia_escolhido === 5) {
+    alert("O dia escolhido é Quinta");
+} else if (dia_escolhido === 6) {
+    alert("O dia escolhido é Sexta");
+} else if (dia_escolhido === 7) {
+    alert("O dia escolhido é Sábado");
 } else {
-
+    alert("Dia Inválido");
 }
