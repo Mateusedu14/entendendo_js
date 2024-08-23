d37b908ca521e6e2949552e0b1c289bac28eed38
mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2024-08-23 20:05:04 -0300

    feat: exercício 4 concluído

--- exercicio3.js
+++ exercicio3.js
@@ -4,7 +4,7 @@ let numero1 = parseInt(prompt("Informe o Primeiro Numero:"));
 let numero2 = parseInt(prompt("Informe o Segundo Numero:"));
 
 if(numero1 > numero2){
-    alert("O numero1 é maior que o numero 2.");
+    alert("O numero 1 é maior que o numero 2.");
 } else {
     alert("O numero 2 é maior que numero 1.");
 }
---
+++ exercicio4.js
@@ -0,0 +1,18 @@
+
+//Entrada de dados
+let numero1 = parseInt(prompt("Informe o Primeiro Numero:"));
+let numero2 = parseInt(prompt("Informe o Segundo Numero:"));
+let numero3 = parseInt(prompt("Informe o Terceiro Numero:"));
+
+
+
+if ((numero1 > numero2) && (numero2 > numero3)) {
+    alert("O Numero 1 é o Maior");
+}
+else if ((numero2 > numero1) && (numero2 > numero3)) {
+    alert("O Numero 2 é o Maior");
+
+} else if ((numero3 > numero1) && (numero3 > numero2)) {
+
+    alert("O Numero 3 é o Maior");
+}
