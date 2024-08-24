0b4b0d479ff51bcf217e125148849e9dc37e8442
mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2024-08-23 21:26:11 -0300

    feat: Correção exercício 5

--- exercicio5.js
+++ exercicio5.js
@@ -2,24 +2,30 @@
 let numero1 = parseInt(prompt("Informe o Primeiro Numero:"));
 let numero2 = parseInt(prompt("Informe o Segundo Numero:"));
 let numero3 = parseInt(prompt("Informe o Terceiro Numero:"));
+let maior, menor;
 
-if ((numero1 > numero2) && (numero2 > numero3)) {
-    alert("O Numero 1 é o Maior");
+if ((numero1 >= numero2) && (numero2 >= numero3)) {
+    maior = numero1
 }
-else if ((numero2 > numero1) && (numero2 > numero3)) {
-    alert("O Numero 2 é o Maior");
+else if ((numero2 >= numero1) && (numero2 >= numero3)) {
+    maior = numero2
 
-} else if ((numero3 > numero1) && (numero3 > numero2)) {
-
-    alert("O Numero 3 é o Maior");
+} else if ((numero3 >= numero1) && (numero3 >= numero2)) {
+    maior = numero3
+   
 }
-if ((numero1 < numero2) && (numero2 < numero3)) {
-    alert("O Numero 1 é o Maior");
+if ((numero1 <= numero2) && (numero2 <= numero3)) {
+    menor = numero1
 }
-else if ((numero2 < numero1) && (numero2 < numero3)) {
-    alert("O Numero 2 é o Maior");
+else if ((numero2 <= numero1) && (numero2 <= numero3)) {
+    menor = numero2
 
-} else if ((numero3 > numero1) && (numero3 > numero2)) {
-
-    alert("O Numero 3 é o Maior");
+} else if ((numero3 <= numero1) && (numero3 <= numero2)) {
+    menor = numero3
+    
 }
+
+alert(`O Numero ${maior} é o Maior de todos!
+        E o Numero ${menor} é o menor de todos!`);
+
+        
