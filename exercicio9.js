0444090daadbd925971c14b0a4374e7f4f07dad0
mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2024-08-27 19:18:19 -0300

    feat: exercicio 9 concluído

--- exercicio9.js
+++ exercicio9.js
@@ -2,3 +2,27 @@
 Peça para digitar M- matutino, V- Vespertino ou N- Noturno. 
 Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso digitado. */
 
+let textomenu = `Escolha o turno do usuário
+                M- Matutino
+                V- Vespertino
+                N- Noturno `;
+
+let turnoEscolhido = prompt(textomenu);
+let turnoNome;
+
+switch (turnoEscolhido) {
+    case "M":
+        turnoNome = "Bom Dia!";
+        break;
+    case "V":
+        turnoNome = "Boa Tarde!";
+        break;
+    case "N":
+        turnoNome = "Boa Noite!";
+        break;
+    default:
+        turnoNome = "Turno Inválido";
+        break;    
+
+}
+alert(`${turnoNome}`);
