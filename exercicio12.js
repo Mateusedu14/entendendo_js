9ba9ec7f72cfc080d904e9bf8fd3d8787a06ad5b
mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2024-08-28 19:53:13 -0300

    feat: exercicio 11 concluido

---
+++ exercicio12.js
@@ -0,0 +1,7 @@
+/*
+Faça um algoritmo que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, dezenas e 
+unidades do mesmo. Observando os termos no plural a colocação do "e", da vírgula entre outros.
+Exemplo: 326: 3 centenas, 2 dezenas e 6 unidades.
+*/
+
+let textomenu
